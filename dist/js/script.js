c99d4a79ec335a70d81e2f8deadf5f5bbe20d75d
Abdulaziz <abdulazizfaactory@gmail.com>
2022-06-18 13:27:54 +0500

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+// import beforeEffectslider from './before-effect-slider.js';
+// 
+// console.log(beforeEffectslider);
+
+// document.addEventListener("DOMContentLoaded", () => {
+    
+//     const border = document.querySelectorAll('.nav__item');
+    
+//     function removeBorders() {
+//         border.forEach(element => {
+//             if(element.classList.contains('nav__item_active') && element) {
+//                 element.classList.remove('nav__item_active');
+//             }
+//         });
+//     }
+    
+//     border.forEach(element => {
+//         element.addEventListener('click', () =>{
+//             removeBorders();
+//             element.classList.add('nav__item_active');
+//         })
+//     });
+    
+//     beforeEffectslider({
+//         Selector: "#beforeEffectslider", // Element that the slider will be build in
+//         BeforeImage: "../img/example/before.png", // Before Image
+//         AfterImage: "../img/example/after.png",// After Image,
+//         Border: {
+//             width: 0
+//         },
+//         LineColor:'transparent', //Line size
+//         Buttons: false,
+//     });
+//     });
